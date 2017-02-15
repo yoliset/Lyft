5f6acbc595ec5ddba1d55bbfca8246e2192b6d7a
Lizbeth CCapira <lizbeth9589@gmail.com>
2017-02-15 13:35:22 -0500

    Arreglo en js , para control de posicion en mapa

--- js/mapas.js
+++ js/mapas.js
@@ -7,40 +7,34 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: myLatLng
-  });
+    center: myLatLng,
+         mapTypeControl: true,
+         mapTypeControlOptions: {
+              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+              position: google.maps.ControlPosition.TOP_CENTER
+    },
+         zoomControl: true,
+         zoomControlOptions: {
+              position: google.maps.ControlPosition.LEFT_CENTER
+    },
+         scaleControl: true,
+         streetViewControl: true,
+         streetViewControlOptions: {
+              position: google.maps.ControlPosition.LEFT_TOP
+    }
+  });  
 
   var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
+         position: myLatLng,
+         map: map,
+         title: 'Hello World!'
   });
     
   var image = '/img/nia.png';
   var beachMarker = new google.maps.Marker({
-    position: {lat: -33.890, lng: 151.274},
-    map: map,
-    icon: image
+         position: {lat: -33.890, lng: 151.274},
+         map: map,
+         icon: image
   });
 }
 
-
-
-
-function init(){
-    
-}
-var miMapa;
-function initMap() {
-  map = new google.maps.Map(
-      document.getElementById('map'),
-    { 
-    center: {
-        lat: -16.457389199999998,
-        Ing: -71.5315308
-    };
-    zoom:14
-    }
- );
-}
-    
